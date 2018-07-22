60a3e3db5c94499c273e11bbcb59d7a894943cf4
mustardfoot <mustardfoot@gmail.com>
2018-07-22 01:46:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const Trello = require("node-trello");
+const t = new Trello(process.env.T_KEY,process.env.T_TOKEN);
 const client = new Discord.Client();
 var pref = "!"
 var guild;
@@ -10,6 +12,134 @@ function addcommand(name,aliases,desc,minrank,does){
 
 addcommand("test",["check"],"checks if the bot is online","",function(args,message){
     message.channel.send(":white_check_mark: **The bot is active!**");
+});
+
+addcommand("unmute",[],"unmutes a user who was previously muted","helper",function(args,message){
+  var userlist = message.mentions.members;
+  if(message.member){
+    userlist.forEach(function(user){
+      if (message.member.highestRole.comparePositionTo(user.highestRole) > 0 ) {
+        t.get("/1/boards/5979179aba4cd1de66a4ea5b/lists", function(err, datas) {
+          datas.forEach(function(data){
+            if (data.name === "mutes"){
+              hwids = data.id;
+            }
+          })
+          if(hwids){
+            t.get("/1/lists/"+hwids+"/cards?fields=id,name,desc",function(err,cards){
+              cards.forEach(function(card){
+                if (card.name === user.id){
+                  t.del('1/cards/'+card.id,function(err,returns){
+                    if(err){
+                      console.log(err);
+                    }
+                  });
+                }
+              })
+            });
+          }else{
+            message.channel.send("**:no_entry_sign: Something seems to be wrong with the mute databases, and the user could not be unmuted properly.**")
+          }
+        });
+        var roles = user.roles
+        roles.forEach(function(role){
+          if (role.name === "muted") {
+            user.removeRole(role)
+            message.channel.send("**:white_check_mark: <@"+user.id+"> has been successfully unmuted.**")
+            user.user.createDM().then((boi) => {
+              boi.send("**:white_check_mark: You have been unmuted.**")
+            });
+          }
+        })
+      }else{
+        message.channel.send("**:no_entry_sign: You do not have permission to moderate this user.**")
+      }
+    })
+  }
+});
+
+addcommand("mute",[],"prevents the mentioned user from talking in text and voice channels","helper",function(args,message){
+  var userlist = message.mentions.members;
+  var muser = null;
+  var curnum = 0;
+  userlist.forEach(function(thatoneguythatgotmentioned){
+    if(muser === null){
+      muser = thatoneguythatgotmentioned;
+    }
+  });
+  var reason = "No Reason Provided";
+  args.forEach(function(role){
+    if(curnum > 2){
+      if(curnum === 3){
+        reason = role;
+      }else{
+        reason = reason+" "+role;
+      }
+    }
+    curnum = curnum+1;
+  })
+  if (muser){
+    if (message.member) {
+      if(message.member.highestRole.comparePositionTo(muser.highestRole) > 0){
+        if (message.member.guild.roles.find("name","muted")) {
+          muser.addRole(message.member.guild.roles.find("name","muted"))
+          var today = new Date();
+          var m = today.getMinutes();
+          if (args) {
+            if (!args[2] || !parseInt(args[2])){
+              args[2] = 0;
+            };
+            t.get("/1/boards/5979179aba4cd1de66a4ea5b/lists", function(err, datas) {
+              datas.forEach(function(data){
+                if (data.name === "mutes"){
+                  hwids = data.id;
+                }
+              })
+              if(hwids){
+                t.get("/1/lists/"+hwids+"/cards?fields=id,name,desc",function(err,cards){
+                  cards.forEach(function(card){
+                    if(card.name === muser.id){
+                      t.del('1/cards/'+card.id,function(err,returns){
+                        if(err){
+                          console.log(err);
+                        }
+                      });
+                    }
+                  })
+                  t.post('/1/cards?name='+muser.id+'&desc='+args[2]+'&pos=top&idList='+hwids,function(err,returns){
+                    if(err){
+                      console.log(err);
+                    }
+                  });
+                });
+              }else{
+                message.channel.send("**:no_entry_sign: Something seems to be wrong with the mute databases, automatic unmute will not work.**")
+              }
+          });
+          }
+          var time = "Forever"
+          if (args && parseInt(args[2])){
+            time = parseInt(args[2])+" Minutes";
+          }
+          if(time === "Forever"){
+            cmdoutput("**<@"+muser.id+"> has been muted forever.**",message.channel)
+            muser.muser.createDM().then((boi) => {
+              boi.send("**:no_entry_sign: You have been muted forever.**")
+            });
+          }else{
+            cmdoutput("**<@"+muser.id+"> has been muted by <@"+message.user.id+"> for "+time+" minutes.**",message.channel)
+            muser.muser.createDM().then((boi) => {
+              boi.send("**:no_entry_sign: You have been muted for "+time+" minutes.**")
+            });
+          }
+        } else {
+          message.channel.send("**:no_entry_sign: We cannot mute the user due to there being no muted role.**")
+        }
+      }else{
+        message.channel.send("**:no_entry_sign: You do not have permission to moderate this user.**")
+      }
+    }
+  }
 });
 
 addcommand("verify",[],"verifies an unverified user","",function(args,message){
