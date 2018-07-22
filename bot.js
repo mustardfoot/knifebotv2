1c5abcb6fff610a73ca58d2d04a5f49f2580dd25
mustardfoot <mustardfoot@gmail.com>
2018-07-22 17:40:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -112,7 +112,7 @@ addcommand("mute",[],"prevents the mentioned user from talking in text and voice
                       t.del('1/cards/'+card.id,function(err,returns){});
                     }
                   })
-                  t.post('/1/cards?name='+mentionedmember.id+'&desc='+args[2]+'&pos=top&idList='+hwids,function(err,returns){
+                  t.post('/1/cards?name='+mentionedmember.id+'&desc='+time+'&pos=top&idList='+hwids,function(err,returns){
                     if(guild.roles.find("name","muted")){
                       var good = true;
                       mentionedmember.addRole(guild.roles.find("name","muted"))
