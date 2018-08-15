f094b11f23f84df40117821923a0fcb2ca6caaba
mustardfoot <mustardfoot@gmail.com>
2018-08-14 22:35:37 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -839,8 +839,7 @@ client.on('messageUpdate', (omessage, message) => {
 });
 
 client.on('messageReactionAdd', (reaction, user) => {
-    console.log(reaction.channel,reaction.channel.name,reaction.channel.name === "nsfw-verification")
-    if(reaction.channel && reaction.channel.name && reaction.channel.name === "nsfw-verification"){
+    if(reaction.message.channel && reaction.message.channel.name && reaction.message.channel.name === "nsfw-verification"){
       console.log(reaction.emoji.name);
       if(reaction.emoji.name === "✅") {
         reaction.users.forEach(function(user){
